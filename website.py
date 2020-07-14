2f41c678d08f6ad8216388565871dfbab92b6543
snroeust <robert.eustergerling@stud.uni-due.de>
2020-07-14 19:30:36 +0200

    Flask FPVHD setup

--- website.py
+++ website.py
@@ -49,5 +49,5 @@ if __name__ == '__main__':
     scheduler.init_app(app)
     scheduler.start()
 
-    app.run(host="0.0.0.0", port=80, debug=True,
+    app.run(host="127.0.0.1", port=5000, debug=True,
             threaded=True, use_reloader=False)
